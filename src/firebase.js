bc53deea96c90ea6d5759f948c54f8b35320537e
ahmed7gendy <106438956+ahmed7gendy@users.noreply.github.com>
2024-08-26 14:04:39 +0300

    Initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,14 @@ import 'firebase/storage';
 
 // استبدل هذه القيم بالقيم الخاصة بمشروعك من Firebase Console
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID'
+    apiKey: "AIzaSyA2sikiw2_wYZ0jE5v62dj021Gu33b78eM",
+    authDomain: "new-e-learning-edecs.firebaseapp.com",
+    databaseURL: "https://new-e-learning-edecs-default-rtdb.firebaseio.com",
+    projectId: "new-e-learning-edecs",
+    storageBucket: "new-e-learning-edecs.appspot.com",
+    messagingSenderId: "82033132236",
+    appId: "1:82033132236:web:30aa4a6ef16e8251077869",
+    measurementId: "G-CEZJRLPSW9"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
